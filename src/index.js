654d3263060cc40e2c3bb899a193dfe9c8abb402
westher0870 <80432333+westher0870@users.noreply.github.com>
2021-03-11 23:23:34 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -87,6 +87,9 @@ function displayWeather(response) {
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
+      let iconElement=document.querySelector("#icon");
+   iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("alt",response.data.weather[0].description);
 }
 
 function showPosition(event) {
